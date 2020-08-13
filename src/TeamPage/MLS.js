77c022f0e450d183bd39c6e63a09f7df7eabdec4
Jrperez79 <joelrperez@gmail.com>
2020-08-13 07:44:03 -0700

    fixed Boolean issue

--- src/TeamPage/MLS.js
+++ src/TeamPage/MLS.js
@@ -7,10 +7,10 @@ export default class MLS extends Component {
     } = this.props
     return (
       <li key={Math.random()} className="mls">
-        <h1>{data.name}</h1>
-        <p>League Standing: {data.league_standing}</p>
-        <p>Ever Won a Championship: {data.ever_won_a_championship}</p>
+        <h2>{data.name}</h2>
         <p>{data.conference} Conference</p>
+        <p>League Standing: {data.league_standing}</p>
+        <p>Ever Won a Championship: {data.ever_won_a_championship ? 'True' : 'False'}</p>
       </li>
     )
   }
